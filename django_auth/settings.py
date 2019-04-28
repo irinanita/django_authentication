82c664f4bed3c43ecd31073fdc71ff748d0fdc6e
Irina Nita <irinanita.im@gmail.com>
2019-04-28 13:51:09 +0000

    Sending email to an actual address

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -121,4 +121,11 @@ STATIC_URL = '/static/'
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+#EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
+EMAIL_HOST_PASSWORD = os.environ.get('PASSWORD')
+#Remember to activate access for less secure apps in Google Security as well 
